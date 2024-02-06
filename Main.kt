434a66d39c76badea2c5470b411677a0de59f68d
mutesi aline <alinemutesiii@gmail.com>
2024-02-06 23:02:58 +0300

    added one kotlin file

---
+++ Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var fullName = "Mutesi Aline"
+    println(fullName)
+
+    var age = 23
+    println(age)
+
+    var phoneNumber = 786766391
+    println(phoneNumber)
+
+    var weight = 61.5
+    println(weight)
+
+    var citizen = "rwandan"
+    println(citizen)
+
+}
